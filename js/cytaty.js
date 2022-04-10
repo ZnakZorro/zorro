bc21d34e30fdce6c289c7799171926d59e3750fd
Znak Zorro <ZnakZorro@users.noreply.github.com>
2022-04-10 10:07:44 +0200

    Update cytaty.js

--- js/cytaty.js
+++ js/cytaty.js
@@ -1,18 +1,22 @@
 
 console.log("cytaty.js");
 
-fetch("./data/cytaty.txt")
-.then(r => {return r.text()})
-.then(txt => {
-    console.log(txt);
-    let arr = txt.split("\n");
-    console.log(arr,arr.length);
-    let los = Math.ceil(Math.random()*arr.length);
-    let elem = document.createElement("div");
-    let cytat = arr[los];
-    console.log(los,cytat);
-    elem.style.cssText = "color:#800";
-    elem.innerHTML = "cytat: "+cytat;
-    document.body.appendChild(elem);  
-})
-.catch(e => {console.log(e)})
+document.addEventListener("DOMContentLoaded",function(){
+    
+    fetch("./data/cytaty.txt")
+    .then(r => {return r.text()})
+    .then(txt => {
+        console.log(txt);
+        let arr = txt.split("\n");
+        console.log(arr,arr.length);
+        let los = Math.ceil(Math.random()*arr.length);
+        let elem = document.createElement("div");
+        let cytat = arr[los];
+        console.log(los,cytat);
+        elem.style.cssText = "color:#800";
+        elem.innerHTML = "cytat: "+cytat;
+        document.body.appendChild(elem);  
+    })
+    .catch(e => {console.log(e)})
+    
+}); // EOF DOM ready
