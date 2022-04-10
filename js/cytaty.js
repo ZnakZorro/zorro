3fcde5f8ac408781b2881d00d98f8a9b0095d6a6
Znak Zorro <ZnakZorro@users.noreply.github.com>
2022-04-10 10:09:32 +0200

    Update cytaty.js

--- js/cytaty.js
+++ js/cytaty.js
@@ -9,13 +9,13 @@ document.addEventListener("DOMContentLoaded",function(){
         console.log(txt);
         let arr = txt.split("\n");
         console.log(arr,arr.length);
-        let los = Math.ceil(Math.random()*arr.length);
+        let los = Math.ceil(Math.random() * (arr.length-1));
         let elem = document.createElement("div");
         let cytat = arr[los];
         console.log(los,cytat);
         elem.style.cssText = "color:#800";
         elem.innerHTML = "cytat: "+cytat;
-        document.body.appendChild(elem);  
+        if (cytat) document.body.appendChild(elem);  
     })
     .catch(e => {console.log(e)})
     
