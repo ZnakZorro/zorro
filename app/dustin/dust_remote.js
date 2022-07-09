cc9119eef07613997ecb9b58aa3b1d408c9bc442
Znak Zorro <ZnakZorro@users.noreply.github.com>
2022-07-09 12:45:03 +0200

    Update dust_remote.js

--- app/dustin/dust_remote.js
+++ app/dustin/dust_remote.js
@@ -1,11 +1,19 @@
 console.log("dust_remote.js");
 
+const $=e=>document.querySelector(e);
+const $$=e=>document.querySelectorAll(e);
+
 let elem = document.createElement('div');
 elem.style.cssText = "padding:0.25em;background:#006; color:#fff";
 elem.innerHTML = "<p>dust_remote.js</p>";
 document.body.appendChild(elem);
 
-document.getElementById("chart-container").style.width = "100%";
-document.querySelector("div.highcharts-container").style.width = "100%";
-document.querySelector("svg").style.width = "100%";
-document.querySelector("svg").width = "100%";
+
+//---dom ready--------------------------------
+document.addEventListener("DOMContentLoaded",function(){
+    document.getElementById("chart-container").style.width = "100%";
+    document.querySelector("div.highcharts-container").style.width = "100%";
+    document.querySelector("svg").style.width = "100%";
+    document.querySelector("svg").width = "100%";
+})
+//---DOM READY----------------------------------
