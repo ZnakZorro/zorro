90152398570c7fb7fec6e06ad065edc05ff33a2c
Znak Zorro <ZnakZorro@users.noreply.github.com>
2022-07-09 12:37:43 +0200

    Update dust_remote.js

--- app/dustin/dust_remote.js
+++ app/dustin/dust_remote.js
@@ -1,6 +1,6 @@
 console.log("dust_remote.js");
 
 var elem = document.createElement('div');
-elem.style.cssText = 'background:#006'; color:#fff;
+elem.style.cssText = "background:#006; color:#fff";
 elem.innerHTML = "<p>dust_remote.js</p>";
 document.body.appendChild(elem);
