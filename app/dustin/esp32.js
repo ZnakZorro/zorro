eecb65c7a27de8d29279b3cc42df69d212c14f0b
Znak Zorro <ZnakZorro@users.noreply.github.com>
2022-07-10 06:49:38 +0200

    Create esp32.js

---
+++ app/dustin/esp32.js
@@ -0,0 +1,16 @@
+console.log("1 esp.js from github");
+
+const $=e=>document.querySelector(e);
+const $$=e=>document.querySelectorAll(e);
+
+let elem = document.createElement('div');
+elem.id="info";
+elem.style.cssText = "padding:0.25em;background:#006; color:#fff";
+elem.innerHTML = "<p>esp32.js</p>";
+document.body.appendChild(elem);
+
+//---dom ready--------------------------------
+document.addEventListener("DOMContentLoaded",function(){
+        
+})
+//---DOM READY----------------------------------
