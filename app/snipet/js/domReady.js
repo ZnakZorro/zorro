cf1a425ef2cb05ea266fc92abcd62c5ce8a97946
Znak Zorro <ZnakZorro@users.noreply.github.com>
2022-07-26 11:59:33 +0200

    Create domReady.js

---
+++ app/snipet/js/domReady.js
@@ -0,0 +1,14 @@
+
+const $=e=>document.querySelector(e);
+const $$=e=>document.querySelectorAll(e);
+
+(() => {
+   
+})();
+
+//---dom ready--------------------------------
+document.addEventListener("DOMContentLoaded",function(){
+    $("#diva").style.color="#222";
+    $("#diva").style.backgroundColor="#eee";
+})
+//---DOM READY----------------------------------
