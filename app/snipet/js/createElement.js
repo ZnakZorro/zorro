02297096a8da84b484682b6c3345a8898efb4303
Znak Zorro <ZnakZorro@users.noreply.github.com>
2022-07-28 07:36:37 +0200

    Update createElement.js

--- app/snipet/js/createElement.js
+++ app/snipet/js/createElement.js
@@ -1,3 +1,20 @@
+let newDiv = document.createElement('div');
+    newDiv.setAttribute('data-id','id1'); 
+    newDiv.classList.add('foo');
+    newDiv.classList.toggle('foo');
+    newDiv.addEventListener('click', modifyText, false); 
+document.body.appendChild(newDiv);
+
+let klawisz = document.createElement('button');
+    klawisz.addEventListener('click', modifyText, false); 
+document.body.appendChild(klawisz);
+
+let container = document.querySelector('nav');
+let doc = 'nowy wpis';
+container.insertAdjacentHTML('beforeend', doc);
+//beforebegin afterbegin beforeend afterend
+
+
 const toggle = ele => (ele.style.display = (ele.style.display === 'none') ? 'block' : 'none');
 const insertAfter = (ele, anotherEle) => anotherEle.insertAdjacentElement('afterend', ele);
 const stripHtml = html => (new DOMParser().parseFromString(html, 'text/html')).body.textContent || ''
@@ -5,25 +22,7 @@ let striper = stripHtml("halo halo");
 console.log(striper);
 
 
-let container = document.querySelector('nav');
-let doc = 'nowy wpis';
-container.insertAdjacentHTML('beforeend', doc);
-//beforebegin afterbegin beforeend afterend
  
-
-var klawisz = document.createElement('button');
-    klawisz.addEventListener('click', modifyText, false); 
-document.body.appendChild(klawisz);
-
-
-var newDiv = document.createElement('div');
-    newDiv.setAttribute('data-id','id1'); 
-    newDiv.classList.add('foo');
-    newDiv.classList.toggle('foo');
-    newDiv.addEventListener('click', modifyText, false); 
-document.body.appendChild(newDiv);
-
-
 
 console.time("jsdiv");
 var c = document.createDocumentFragment();
