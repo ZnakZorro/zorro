e6c9f4e219e53aa4c1fe53a8992b9dd457b4516a
Znak Zorro <ZnakZorro@users.noreply.github.com>
2022-08-01 09:18:40 +0200

    Update edit.js

--- js/edit.js
+++ js/edit.js
@@ -1,1 +1,14 @@
 
+const formatTXT2HTML=(code)=>{
+   let arr = code.split("\n");
+   if (arr[0]) arr[0] = "<h3>"+arr[0]+"</h3>";
+   if (arr[1]) arr[1] = "<b>"+arr[1]+"</b>";
+   if (arr[2]) arr[2] = "<b><i>"+arr[2]+"</i></b>";
+   for (let i=1; i<arr.length-1; i++){
+      arr[i] = arr[i].trim();
+      if (arr[i] != "") arr[i] = "<p>"+arr[i]+"</p>";
+      else              arr[i] = "<br />";
+   }
+   //console.log("aaaaaaa=",arr);
+   return arr.join("\n");
+}
