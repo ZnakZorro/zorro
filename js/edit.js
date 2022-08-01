bcf946092f8e25474952dffeee3596e7835b8698
Znak Zorro <ZnakZorro@users.noreply.github.com>
2022-08-01 09:22:49 +0200

    Update edit.js

--- js/edit.js
+++ js/edit.js
@@ -1,5 +1,6 @@
-
-const formatTXT2HTML=(code)=>{
+console.log("edit.js");
+            
+const formatTXT2HTML=(code,ext="txt",type=null)=>{
    let arr = code.split("\n");
    if (arr[0]) arr[0] = "<h3>"+arr[0]+"</h3>";
    if (arr[1]) arr[1] = "<b>"+arr[1]+"</b>";
