653fb0ea04fe226fce3778173255ee7030ba9812
Znak Zorro <ZnakZorro@users.noreply.github.com>
2022-08-02 07:43:12 +0200

    Create dir.js

---
+++ app/snipet/dir.js
@@ -0,0 +1,21 @@
+const dir = [
+   {n:"Gramofon",        f:"dom",     v:"html",  u:"gramofon.htm"},
+   {n:"Szlif",           f:"dom",     v:"html",  u:"szlif.htm"},
+   {n:"Ekspres",         f:"dom",     v:"txt",   u:"expres.txt"},
+   {n:"Nalewki",         f:"dom",     v:"txt",   u:"nalewki.txt"},
+   {n:"index",           f:"html",    v:"code",  u:"index.html"},
+   {n:"git",             f:"html",    v:"code",  u:"git.html"},
+   {n:"fetch",           f:"js",      v:"code",  u:"fetch.js"},
+   {n:"createElement",   f:"js",      v:"code",  u:"createElement.js"},
+   {n:"active",          f:"js",      v:"code",  u:"active.js"},
+   {n:"dom",             f:"js",      v:"code",  u:"domReady.js"},
+   {n:"param",           f:"js",      v:"code",  u:"parameters.js"},
+   {n:"Borówki",         f:"580",     v:"txt",   u:"borowki.txt"},
+   {n:"Ciasto",          f:"kuchnia", v:"txt",   u:"ciasto.txt"},
+   {n:"Piernik",         f:"kuchnia", v:"html",  u:"Piernik.htm"},
+   {n:"Lody",         f:"kuchnia",     v:"md",   u:"lody.md"},
+   {n:"Lody2",         f:"kuchnia",     v:"md",   u:"lody2.md"},
+   {n:"md",         f:"md", v:"md",  u:"md.md"},
+   
+   
+];
