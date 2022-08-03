77282cb758cd68ee3b21a982b0d1cacf1f018a81
Znak Zorro <ZnakZorro@users.noreply.github.com>
2022-08-03 15:02:54 +0200

    Create console.js

---
+++ app/snipet/code/console.js
@@ -0,0 +1,6 @@
+
+<script src="//cdn.jsdelivr.net/npm/eruda"></script>
+<script>eruda.init();</script>
+
+javascript:(function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() } })();
+
