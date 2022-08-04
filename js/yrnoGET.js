38e4b544410e415fe0853a907897221f08c47599
Znak Zorro <ZnakZorro@users.noreply.github.com>
2022-08-04 10:43:20 +0200

    Update yrnoGET.js

--- js/yrnoGET.js
+++ js/yrnoGET.js
@@ -1,6 +1,12 @@
 let yrnoToken = ()=>{return(new Date()).getTime();}
 let _city = "Szczecin-Dąbie";
 let _dataType = "compact"; //  compact  complete   classic
+let APPcity = localStorage.getItem("APPcity")
+let APPlat = localStorage.getItem("APPlat");
+let APPlon = localStorage.getItem("APPlon");
+let APPalt = localStorage.getItem("APPlat");
+console.log(APPcity,APPlat,APPlon,APPalt);
+
 let _urlYRNO = "https://api.met.no/weatherapi/locationforecast/2.0/"+_dataType+"?lat=53.378773&lon=14.665842&altitude=25";//&t="+yrnoToken();
 //let yrnoPL={};
 
