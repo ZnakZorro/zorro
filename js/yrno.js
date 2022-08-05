5e5d248ff7515b3135c2521a551f38222374720b
Znak Zorro <ZnakZorro@users.noreply.github.com>
2022-08-05 11:14:37 +0200

    Update yrno.js

--- js/yrno.js
+++ js/yrno.js
@@ -244,7 +244,7 @@ console.log("235 apparentTemperature(w.t, w.wind, w.humid, w.press)=",w.t, w.win
 
 //fffffffffffffffffffffffffffffffffffffff
 //const formatLine=(w)=> `<span title="chill=${w.chill}">${w.time}</span><span>${w.icon}</span><span>${w.temp}</span><br /><span>${tosm(w.press,"hPa")}</span><br /><span>${ronda(w.wind,"m/s")}, ${ronda(w.humid,"%")}</span>`;
-const formatLine=(w)=> `<span>${w.time}</span><span>${w.icon}</span><span>${w.temp}<br />${w.chill}</span><br /><span>${tosm(w.press,"hPa")}</span><br /><span>${ronda(w.wind,"m/s")}, ${ronda(w.humid,"%")}</span>`;
+const formatLine=(w)=> `<span>${w.time}</span><span>${w.icon}</span><span>${w.temp}<br /><span class="g">${w.chill}</span></span><br /><span>${tosm(w.press,"hPa")}</span><br /><span>${ronda(w.wind,"m/s")}, ${ronda(w.humid,"%")}</span>`;
 
   const getYRNOhour=(nr=0,id)=>{ 
       let w = getOBJhour(nr);
