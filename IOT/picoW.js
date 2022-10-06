498c6b6ab43ef346a73b1b9a42bd2092ebb0083b
Znak Zorro <ZnakZorro@users.noreply.github.com>
2022-10-06 07:53:33 +0200

    Create picoW.js

---
+++ IOT/picoW.js
@@ -0,0 +1,11 @@
+const $=e=>document.querySelector(e);
+const $$=e=>document.querySelectorAll(e);
+
+
+
+document.addEventListener("DOMContentLoaded",function(){
+    //$("#diva").style.color="#222";
+    //$("#diva").style.backgroundColor="#eee";
+    console.log("picoW.js");
+})
+
