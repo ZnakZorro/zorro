b0d053be9e94438fa402f5f7ef6587ac89230179
Znak Zorro <ZnakZorro@users.noreply.github.com>
2022-12-20 07:43:56 +0100

    Update dlna.js

--- IOT/dlna/dlna.js
+++ IOT/dlna/dlna.js
@@ -2,9 +2,23 @@ console.log("dnla.js")
 const $=e=>document.querySelector(e);
 const $$=e=>document.querySelectorAll(e);
 
+const go=(n)=>{
+    console.log(n);
+}
+
+
 //---dom ready--------------------------------
 document.addEventListener("DOMContentLoaded",function(){
     $("#plus").style.color="#008";
     $("#plus").style.backgroundColor="#880";
+let u="https://znakzorro.github.io/zorro/IOT/dlna/dlna.html"    
+fetch(u)
+.then(r => {return r.text()})
+.then(o => {
+    console.log(o)
+    $("#plus").innerHTML=o;
+})
+.catch(e => {console.log(e)})    
+    
 })
 //---DOM READY----------------------------------
