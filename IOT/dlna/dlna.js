774e096118770ec67bf857b0dec5d23055149ba6
Znak Zorro <ZnakZorro@users.noreply.github.com>
2022-12-20 06:53:36 +0100

    Create dlna.js

---
+++ IOT/dlna/dlna.js
@@ -0,0 +1,10 @@
+console.log("dnla.js")
+const $=e=>document.querySelector(e);
+const $$=e=>document.querySelectorAll(e);
+
+//---dom ready--------------------------------
+document.addEventListener("DOMContentLoaded",function(){
+    $("#plus").style.color="#008";
+    $("#plus").style.backgroundColor="#880";
+})
+//---DOM READY----------------------------------
