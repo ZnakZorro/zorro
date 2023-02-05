34ece58783ae3e22b701817bf60d76c82cc2a036
Znak Zorro <ZnakZorro@users.noreply.github.com>
2023-02-05 09:57:53 +0100

    Update module-1.js

--- html/module/module-1.js
+++ html/module/module-1.js
@@ -1,13 +1,7 @@
-// Create a variable named "bestClub":
-const bestClub = "Your Club";
-
-// Create a function named "multiply":
+const bestClub = 'Your Club';
+const fruits = ['Grape', 'Apple', 'Pineapple', 'Lemon'];
 function multiply(x, y) {
   return x * y;
 }
 
-// Create an array named "fruits":
-const fruits = ["Mango", "Apple", "Orange", "Lemon"];
-
-// Export the three statements above:
-export { bestClub, multiply, fruits };
+export { bestClub, fruits, multiply };
