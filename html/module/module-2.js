8a01435eefe3ebbd5dff5fa6d16bfefe26df47b3
Znak Zorro <ZnakZorro@users.noreply.github.com>
2023-02-05 10:01:29 +0100

    Update module-2.js

--- html/module/module-2.js
+++ html/module/module-2.js
@@ -2,9 +2,10 @@ import {
   bestClub as favoriteTeam,
   fruits as crops,
   multiply as product,
+  strona as url
 } from './module-1.js';
 
-const bestClub = `I bought ${product(2, 11)} ${crops[2]}s at ${favoriteTeam}.`;
+const bestClub = `I bought ${product(2, 11)} ${crops[2]}s at ${favoriteTeam}. ${url}`;
 
 console.log(bestClub);
 document.querySelector("#info").innerHTML = bestClub;
