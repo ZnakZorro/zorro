1d48ba928ff79b0782ae660daa92fb6b750e392c
Znak Zorro <ZnakZorro@users.noreply.github.com>
2023-02-05 10:09:32 +0100

    Create script.js

---
+++ html/module/script.js
@@ -0,0 +1,7 @@
+import {
+  bestClub1 as favoriteTeam,
+  fruits1 as crops,
+  multiply1 as product,
+  strona1 as url
+} from './module-1.js';
+document.querySelector("#info1").innerHTML += bestClub1;
