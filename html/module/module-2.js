55b0c3eb967ec4f72b14601375a4c5898ef4ad77
Znak Zorro <ZnakZorro@users.noreply.github.com>
2023-02-05 09:57:19 +0100

    Create module-2.js

---
+++ html/module/module-2.js
@@ -0,0 +1,9 @@
+import {
+  bestClub as favoriteTeam,
+  fruits as crops,
+  multiply as product,
+} from './module-1.js';
+
+const bestClub = `I bought ${product(2, 11)} ${crops[2]}s at ${favoriteTeam}.`;
+
+console.log(bestClub);
