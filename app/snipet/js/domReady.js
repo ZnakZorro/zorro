f3ca250b23a96f6001ec7b1630554a9b2ac952f0
Znak Zorro <ZnakZorro@users.noreply.github.com>
2023-02-11 08:25:36 +0100

    Update domReady.js

--- app/snipet/js/domReady.js
+++ app/snipet/js/domReady.js
@@ -1,3 +1,5 @@
+    const _$  = document.querySelector.bind(document);
+    const _$$ = document.querySelectorAll.bind(document);
 
 const $=e=>document.querySelector(e);
 const $$=e=>document.querySelectorAll(e);
