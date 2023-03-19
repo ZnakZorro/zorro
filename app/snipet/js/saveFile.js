8af4156e88dc701911d0c8232c87419749fbb905
Znak Zorro <ZnakZorro@users.noreply.github.com>
2023-03-19 16:52:37 +0100

    Update saveFile.js

--- app/snipet/js/saveFile.js
+++ app/snipet/js/saveFile.js
@@ -1,16 +1,11 @@
 
-
-
-    function saveTextAsFile(textToWrite, fileNameToSaveAs, fileType) {
+function saveTextAsFile(textToWrite, fileNameToSaveAs, fileType) {
     let textFileAsBlob = new Blob([textToWrite], { type: fileType });
     let downloadLink = document.createElement('a');
     downloadLink.download = fileNameToSaveAs;
     downloadLink.innerHTML = 'Download File';
-
     if (window.webkitURL != null) {
-        downloadLink.href = window.webkitURL.createObjectURL(
-            textFileAsBlob
-        );
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
     } else {
         downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
         downloadLink.style.display = 'none';
@@ -22,14 +17,14 @@
 
 function saveTextAsFile2(text, filename, type = "text/plain") {
     Object.assign(document.createElement('a'), {
-         download: filename,
-         href: URL.createObjectURL(new Blob([text], { type }))
+        download: filename,
+        href: URL.createObjectURL(new Blob([text], { type }))
     }).click();
 }
 
+// example calls
 saveTextAsFile('Hello World!', 'hello.txt', 'text/plain');
 saveTextAsFile('{"hello": "world"}', 'hello.json', 'application/json');
 
 saveTextAsFile2('Hello World!', 'hello.txt', 'text/plain');
 saveTextAsFile2('{"hello": "world"}', 'hello.json', 'application/json');
-
