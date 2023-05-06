32b54e7fb23c33a6828c767efd4f0350fa8ca4fb
Znak Zorro <ZnakZorro@users.noreply.github.com>
2023-05-06 15:26:04 +0200

    Update js.js

--- app/snipet/js/js.js
+++ app/snipet/js/js.js
@@ -1,3 +1,9 @@
 const zeroPad = (num, places) => String(num).padStart(places, '0');
 console.log(zeroPad(5, 4)); // "0005"
 
+Number.prototype.pad = function(size) {
+    let s = String(this);
+    while (s.length < (size || 2)) {s = "0" + s;}
+    return s;
+}
+(9).pad();  //returns "09"
