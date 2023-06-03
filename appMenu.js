ec08c58b587624e9a07afb0cd27950b3ec7ebffe
Znak Zorro <ZnakZorro@users.noreply.github.com>
2023-06-03 06:31:16 +0200

    Update appMenu.js

--- appMenu.js
+++ appMenu.js
@@ -26,20 +26,19 @@ class NavBarNavigator extends HTMLElement {
 		</style>		
 		 <header class="header hidenow" style="background:${color}">
 			 <nav class="navbar">            
-				 <img src="https://znakzorro.github.io/zorro/css/icon/back.svg" width="32" onclick="window.location.href='https://znakzorro.github.io/zorro/';" class="icon1">
+				 <img src="https://znakzorro.github.io/zorro/css/icon/back.svg" width="32" onclick="history.back();" class="icon1">
 				 <a href="https://znakzorro.github.io/zorro/" class="nav-logo">${title}<span id="licznik"></span><span id="flag"></span></a>
 					 <ul class="nav-menu">
-					 <li class="nav-item"><a href="${link}app/waluty/index.html" class="nav-link">WALUTY</a></li>
-					 <li class="nav-item"><a href="${link}app/covid/index.html" class="nav-link">COVID</a></li>
+					 <li class="nav-item"><a href="${link}app/waluty/index.html" class="nav-link">WALUTY</a></li>					 
 					 <li class="nav-item"><a href="${link}app/todo/index.html" class="nav-link">ToDo</a></li>
-					 <li class="nav-item"><a href="${link}html/index.html" class="nav-link">Inne</a></li>
-					 <li class="nav-item"><a href="${link}nav/index.html" class="nav-link">NAV</a></li>
-					 <li class="nav-item"><a href="${link}app/EDEK/" class="nav-link">EDEK</a></li>
-					 <li class="nav-item"><a href="${link}app/snipet/" class="nav-link">SNIPET</a></li>
 					 <li class="nav-item"><a href="${link}html/zapis.html" class="nav-link">Zapis</a></li>
-					 <li class="nav-item"><a href="${link}app/radio/index.html" class="nav-link">LINKI</a></li>   
-					 
+					 <li class="nav-item"><a href="${link}nav/index.html" class="nav-link">NAV</a></li>
+					 <li class="nav-item"><a href="${link}html/index.html" class="nav-link">Inne</a></li>					 
+					 <li class="nav-item"><a href="${link}app/EDEK/" class="nav-link">EDEK</a></li>
+					 <li class="nav-item"><a href="${link}app/snipet/" class="nav-link">SNIPET</a></li>					 
+					 <li class="nav-item"><a href="${link}app/radio/index.html" class="nav-link">LINKI</a></li>					 
 					 <li class="nav-item"><a href="${link}app/ele/index.html" class="nav-link">Ele</a></li>
+					 <li class="nav-item"><a href="${link}app/covid/index.html" class="nav-link">COVID</a></li>
 					 <li class="nav-item"><a href="${link}app/hash/index.html" class="nav-link red">#</a></li>
 					 <li class="nav-item"><a href="${link}app/admin/index.html" class="nav-link">Admin</a></li>
 					 
