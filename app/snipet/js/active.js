3c84fd8b60bf22b055cc4242c525b6559a67553a
Znak Zorro <ZnakZorro@users.noreply.github.com>
2023-06-28 16:23:03 +0200

    Update active.js

--- app/snipet/js/active.js
+++ app/snipet/js/active.js
@@ -1,2 +1,10 @@
+const setActive=(ten)=>{
+      let parent = ten.parentElement;
+      let keys   = parent.querySelectorAll("button");
+      keys.forEach(key=>key.classList.remove("active"));
+      ten.classList.add("active");
+}
+
+
       document.querySelectorAll("#klawisze button").forEach(b=>b.classList.remove("active"));
       ten.classList.add("active");
