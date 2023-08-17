bff238febc5e5d5af4857a9e08c40709deea854b
Znak Zorro <ZnakZorro@users.noreply.github.com>
2023-08-17 13:40:05 +0200

    Update jplayer.js

--- IOT/jplayer/jplayer.js
+++ IOT/jplayer/jplayer.js
@@ -1,6 +1,6 @@
 console.log("git iot jpayer");
 let app = document.querySelector("#app");
-app.innerHTML = `
+app.innerHTML += `
 <h2>jPlayer from js</h2>
 <button onclick="./">/self</button>
 <button onclick="outURL = "http://192.168.31.145/">/jPlayer</button>
@@ -12,7 +12,7 @@ fetch(u)
 .then(r => {return r.text()})
 .then(o => {
     console.log(o)
-    $("#app").innerHTML=o;
+    $("#app").innerHTML+=o;
 })
 .catch(e => {console.log(e)})    
     
