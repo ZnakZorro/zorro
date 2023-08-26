a140c013fb620afaf6496ac1aa6facfdc8dac517
Znak Zorro <ZnakZorro@users.noreply.github.com>
2023-08-26 08:17:00 +0200

    Update jplayer.js

--- IOT/jplayer/jplayer.js
+++ IOT/jplayer/jplayer.js
@@ -34,15 +34,46 @@ document.addEventListener("DOMContentLoaded",function(){
 
 const edit=(ten)=>{
     console.log(ten.textContent);
+    postData("./load", {file: "index.html" }).then((data) => {
+      console.log(data); // JSON data parsed by `data.json()` call
+    });
+
 }
 
 const save=(ten)=>{
     if(ten){
         console.log(ten.textContent);
+        postData("./indexHTML", "zapis index.html").then((data) => {
+          console.log(data); // JSON data parsed by `data.json()` call
+        });
     } else {
         return;
     }
 }
+
+
+// Example POST method implementation:
+async function postData(url = "", data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+
+
+
 
 
 /*
