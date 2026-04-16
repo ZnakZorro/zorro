9cd2836ad9395e21cc47d9e8d91beaa556738594
Znak Zorro <ZnakZorro@users.noreply.github.com>
2026-04-16 11:46:15 +0200

    Add files via upload

---
+++ ToDoOne/js/save.js
@@ -0,0 +1,40 @@
+
+document.getElementById('wybierzPlik').addEventListener('change', function(event) {
+  const plik = event.target.files[0]; // Pobieramy pierwszy wybrany plik
+  if (!plik) return;
+
+  const reader = new FileReader();
+
+  // Co ma się stać, gdy plik zostanie już wczytany do pamięci
+  reader.onload = function(e) {
+    const zawartosc = e.target.result;
+    console.log("Treść pliku:", zawartosc);
+    document.getElementById('list-container').innerHTML += zawartosc;
+    console.log(document.getElementById('list-container').innerHTML);
+  };
+
+  // Uruchomienie odczytu jako tekst
+  reader.readAsText(plik);
+});
+
+
+
+async function zapisz(content="Hello",name='notatka.html') {
+  // Otwiera systemowe okno "Zapisz jako"
+  const handle = await window.showSaveFilePicker({
+    suggestedName: name,
+    types: [{
+      description: 'Plik tekstowy',
+      accept: {'text/plain': ['.html']},
+    }],
+  });
+  
+  const writable = await handle.createWritable();
+  await writable.write(content);
+  await writable.close();
+}
+
+
+
+
+
