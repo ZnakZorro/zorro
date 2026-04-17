cbc246aa2ebbc144fe378996f0669a0f0122be97
Znak Zorro <ZnakZorro@users.noreply.github.com>
2026-04-17 09:01:14 +0200

    Add files via upload

--- Alfa/add/mini.js
+++ Alfa/add/mini.js
@@ -25,6 +25,22 @@ themeToggle.addEventListener("click", function() {
     }
 });
 
+//const uid=()=>Math.random().toString(16).slice(8);
+//const uid=()=>Date.now().toString();
+const uid=()=>getIdDate();
+
+const getIdDate=()=>{
+    const now = new Date();
+
+    const Y = now.getFullYear(); // 2026
+    const M = String(now.getMonth() + 1).padStart(2, '0'); // Miesiące są od 0 do 11
+    const D = String(now.getDate()).padStart(2, '0');
+    const h = String(now.getHours()).padStart(2, '0');
+    const m = String(now.getMinutes()).padStart(2, '0');
+    const s = String(now.getSeconds()).padStart(2, '0');
+
+    return `${Y}${M}${D}${h}${m}${s}`;
+}
 
 
 document.querySelector("#home-btn").addEventListener("click", ()=>{location.href="../"});
@@ -35,4 +51,4 @@ document.querySelector("#alfa-btn").addEventListener("click", ()=>{location.href
 
 
 document.title=storageName;
-document.querySelector("h2").textContent = "Lista "+storageName
+document.querySelector("h2").textContent = "Lista "+storageName;
